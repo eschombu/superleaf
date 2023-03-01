148f46c9c7bc3c2ee699afe219312437475fd015
Erik Schomburg <eschomburg@gmail.com>
2023-03-01 13:25:15 -0500

    add stats utils

---
+++ tests/stats/test_circular.py
@@ -0,0 +1,63 @@
+import numpy as np
+import pytest
+
+from superleaf.stats.circular import circmean
+
+
+def _approx_eq(a, b, tol=1e-12):
+        return abs(a - b) <= tol
+
+
+def test_circmean():
+    degrees = np.array([-150, 180, 150, 270, 90, 0, 540, -540])
+    expected_deg = 180
+    radians = np.deg2rad(degrees)
+    expected_rad = np.deg2rad(expected_deg)
+    assert circmean(radians) == expected_rad
+    assert circmean(degrees, high=360) == expected_deg
+
+    degrees, weights = np.array([
+        [-90, 1],
+        [0, 1],
+        [90, 2],
+    ]).T
+    expected = 45
+    assert _approx_eq(circmean(degrees, weights=weights, high=360), expected)
+
+    degrees, weights = np.array([
+        [-40, 1],
+        [0, 1],
+        [40, 3],
+    ]).T
+    equiv_unweighted = []
+    for d, w in zip(degrees, weights):
+        equiv_unweighted.extend([d] * w)
+    assert _approx_eq(circmean(degrees, weights=weights, high=360), circmean(equiv_unweighted, high=360))
+
+    hours = [12, 14, 23, 15]
+    weights = [1, 1, 2, 1]
+    expected = 13
+    expected_weighted = 12.5
+    assert _approx_eq(circmean(hours, low=12, high=24), expected)
+    assert _approx_eq(circmean(hours, weights=weights, low=12, high=24), expected_weighted)
+
+    radians, weights = np.array([
+        [3 * np.pi / 2, 1],
+        [0, 1],
+        [np.pi / 2, 2],
+        [np.nan, 1],
+    ]).T
+    expected = np.pi / 4
+    assert np.isnan(circmean(radians))
+    assert np.isnan(circmean(radians, weights=weights))
+    assert _approx_eq(circmean(radians, weights=weights, nan_policy='ignore'), expected)
+    with pytest.raises(ValueError):
+        circmean(radians, weights=weights, nan_policy='raise')
+
+    radians = np.vstack([radians, radians]).T
+    weights = np.vstack([weights, weights]).T
+    assert _approx_eq(circmean(radians, weights=weights, nan_policy='ignore'), expected)
+
+    expected_array = np.array([expected, expected])
+    result_array = circmean(radians, weights=weights, axis=0, nan_policy='ignore')
+    assert all([_approx_eq(result, exp) for result, exp in zip(result_array, expected_array)])
---
+++ tests/stats/test_count.py
@@ -0,0 +1,43 @@
+import numpy as np
+import pytest
+
+from superleaf.stats.count import CountStat
+
+
+def test_with_total():
+    n = 10
+    N = 100
+    count_stat = CountStat(n, N)
+    assert count_stat.percent == n
+    assert count_stat.fraction == (n / N)
+    assert count_stat.chi2_pval is None
+    with pytest.raises(ValueError):
+        CountStat(4.5, 10)
+    with pytest.raises(ValueError):
+        CountStat(4, 10.5)
+
+
+def test_nan_total():
+    n = 10
+    count_stat = CountStat(n, None)
+    assert np.isnan(count_stat.fraction)
+    assert np.isnan(count_stat.percent)
+
+
+def test_chi2():
+    n = 10
+    N = 100
+    e = 15
+    assert 0 <= CountStat(n, N, expected=e).chi2_pval <= 1
+    assert 0 <= CountStat(n, N, expected=(e / N)).chi2_pval <= 1
+
+
+def test_addition():
+    n1 = 10
+    n2 = 15
+    N = 100
+    cs1 = CountStat(n1, N)
+    cs2 = CountStat(n2, N)
+    cs12 = cs1 + cs2
+    assert cs12.percent == (n1 + n2) / 2
+    assert cs12.fraction == (n1 + n2) / (2 * N)
