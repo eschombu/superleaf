36932bc85b0cd74f2e0ed5f831dfefce9c364fdc
Erik Schomburg <eschomburg@flatironinstitute.org>
2025-05-09 15:37:01 -0400

    add utils/parallel.py and use its parmap to replace the previous sequences.parallel.mapped function

--- src/superleaf/sequences/__init__.py
+++ src/superleaf/sequences/__init__.py
@@ -1,21 +1,30 @@
 from typing import Callable, Iterable, List, TypeVar
 
 from superleaf.sequences.serial import filtered, flatten, flat_map as _flat_map_s, groupby, mapped as _mapped_s
-from superleaf.sequences.parallel import flat_map as _flat_map_p, mapped as _mapped_p
+try:
+    from superleaf.utils.parallel import parmap
+except ImportError:
+    print("Warning: parallel processing not available, please install parallel requirements. "
+          "Falling back to serial processing.")
+    parmap = None
 
 T = TypeVar("T")
 U = TypeVar("U")
 
 
 def mapped(f: Callable[[T], U], seq: Iterable[T], parallel=False, workers=None) -> List[U]:
-    if parallel or (workers is not None and workers > 1):
-        return _mapped_p(f, seq, workers=workers)
+    if parmap is not None and (parallel or (workers is not None and (workers < 0 or workers > 1))):
+        if workers is None:
+            workers = -1
+        return parmap(f, seq, n_workers=workers)
     else:
         return _mapped_s(f, seq)
 
 
 def flat_map(f: Callable, seq: Iterable, depth=None, drop_null=True, parallel=False, workers=None) -> list:
-    if parallel or (workers is not None and workers > 1):
-        return _flat_map_p(f, seq, depth=depth, drop_null=drop_null, workers=workers)
+    if parmap is not None and (parallel or (workers is not None and (workers < 0 or workers > 1))):
+        if workers is None:
+            workers = -1
+        return flatten(parmap(f, seq, n_workers=workers), depth=depth, drop_null=drop_null)
     else:
         return _flat_map_s(f, seq, depth=depth, drop_null=drop_null)
