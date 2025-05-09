3cc63ac12c33d686b644afe519471874f2235c27
Erik Schomburg <eschomburg@flatironinstitute.org>
2025-05-09 12:07:05 -0400

    add default options to getter operators

--- src/superleaf/operators/getters.py
+++ src/superleaf/operators/getters.py
@@ -1,13 +1,23 @@
 from superleaf.operators.base import Operator, operator
 
+_NotSpecified = object()
 
-def attr_getter(name: str) -> Operator:
+
+def attr_getter(name: str, default=_NotSpecified) -> Operator:
     def get(obj):
-        return getattr(obj, name)
+        if default is _NotSpecified:
+            return getattr(obj, name)
+        else:
+            return getattr(obj, name, default)
     return operator(get)
 
 
-def index_getter(index) -> Operator:
+def index_getter(index, default=_NotSpecified) -> Operator:
     def get(obj):
-        return obj[index]
+        try:
+            return obj[index]
+        except KeyError:
+            if default is _NotSpecified:
+                raise
+            return default
     return operator(get)
