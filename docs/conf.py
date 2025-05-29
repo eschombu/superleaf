cd813d3d3bc622617a2066410915e891c599a7e8
Erik Schomburg <eschomburg@gmail.com>
2025-05-29 18:15:08 -0400

    change docs html style and add source code links

--- docs/conf.py
+++ docs/conf.py
@@ -14,6 +14,7 @@ release = '0.2.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
+import inspect
 import os
 import sys
 
@@ -24,6 +25,7 @@ extensions = [
     'sphinx.ext.autodoc',      # core autodoc support
     'sphinx.ext.napoleon',     # for Google/NumPy‑style docstrings
     'sphinx.ext.autosummary',  # generates stub pages automatically
+    'sphinx.ext.linkcode',     # adds link to source code
 ]
 
 templates_path = ['_templates']
@@ -35,10 +37,11 @@ autosummary_generate = True
 autosummary_generate_overwrite = True
 
 autodoc_default_options = {
-    'members': True,            # include all methods & attributes
-    'undoc-members': True,      # even if no docstring
-    'inherited-members': True,  # show methods from base classes
-    'show-inheritance': True,
+    'members': True,             # include all methods & attributes
+    'undoc-members': True,       # even if no docstring
+    'inherited-members': False,  # show methods from base classes
+    'show-inheritance': True,    # insert base classes after the class signature
+    'member-order': 'bysource',  # disable alphabetical sorting
 }
 # For classes, also merge class doc + __init__ doc:
 autoclass_content = 'both'
@@ -46,5 +49,42 @@ autoclass_content = 'both'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = "pydata_sphinx_theme"
+html_context = {
+    "github_user": "eschombu",
+    "github_repo": "superleaf",
+    "github_version": "master",
+    "doc_path": "docs",
+}
+html_theme_options = {
+    "github_url": "https://github.com/eschombu/superleaf",
+    "use_edit_page_button": True,  # Adds an "Edit on GitHub" button
+}
 html_static_path = ['_static']
+
+
+def linkcode_resolve(domain, info):
+    if domain != "py":
+        return None
+    if not info["module"]:
+        return None
+
+    try:
+        module = __import__(info["module"], fromlist=[info["fullname"]])
+        obj = module
+        for part in info["fullname"].split("."):
+            obj = getattr(obj, part)
+        fn = inspect.getsourcefile(obj)
+        source, lineno = inspect.getsourcelines(obj)
+    except Exception:
+        return None
+
+    # Customize for your repo URL and branch
+    repo_url = "https://github.com/eschombu/superleaf"
+    branch = "master"
+
+    # Calculate the relative path from your repository root
+    rel_path = os.path.relpath(fn, start=os.path.abspath(".."))
+
+    # Return URL pointing directly to the source lines on GitHub
+    return f"{repo_url}/blob/{branch}/{rel_path}#L{lineno}-L{lineno + len(source) - 1}"
