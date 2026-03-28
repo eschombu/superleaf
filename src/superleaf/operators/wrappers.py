92603c57bf9b699edb4e1c595a3d9d114c5e8702
Erik Schomburg <eschomburg@flatironinstitute.org>
2026-03-28 16:07:09 -0400

    allow str args for exception in with_fallback wrapper

--- src/superleaf/operators/wrappers.py
+++ src/superleaf/operators/wrappers.py
@@ -2,7 +2,15 @@ from functools import wraps
 from typing import Callable, Optional
 
 
-def with_fallback(f: Optional[Callable] = None, fallback=None, exceptions=Exception):
+def with_fallback(f: Optional[Callable] = None, fallback=None, exceptions: type | str | None = Exception):
+    if isinstance(exceptions, str):
+        if exceptions in ("ignore", "all"):
+            exceptions = Exception
+        elif exceptions in ("raise", "none"):
+            exceptions = None
+        else:
+            raise ValueError(f"Invalid value for exceptions: {exceptions!r}")
+
     def wrapper(fun):
         @wraps(fun)
         def wrapped(*args, **kwargs):
